0bc3e72e997ef0d03ae76cd2839897a494674197
Kuna Lakshmi Vara Prasad <kunalakshmi.prasad@nw18.com>
2025-05-31 18:40:30 +0530

    revert changes

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -21,29 +21,4 @@ function mergeSortedArrays(arr1, arr2) {
     }
     return merge
 }
-
-var merge = function(nums1, m, nums2, n) {
-    let res = []
-    let i = 0
-    let j = 0
-    while(i < m && j < n)
-        if(nums1[i] < nums2[j]) {
-            res.push(nums1[i])
-            i++
-        }
-        else {
-            res.push(nums2[j])
-            j++
-        }
-    while(i < m){
-        res.push(nums1[i])
-        i++
-    }
-    while(j < n){
-        res.push(nums2[j])
-        j++
-    }
-    return res
-};
 console.log(mergeSortedArrays([2, 4, 7, 10], [2, 3]))
-console.log(merge([2, 4, 7, 10],4, [2, 3],2))
